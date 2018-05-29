1cb0c78a0ce2d4ce52940aacf0f120720a2f6c28
yong <zengjunyong@gmail.com>
2018-05-29 16:26:58 +0800

    refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Step1 from '@/components/Step1'
 
 Vue.use(Router)
 
@@ -8,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/ss',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Step1',
+      component: Step1
     }
   ]
 })
